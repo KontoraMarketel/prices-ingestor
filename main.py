13874ece7d50e454abcef2575ec0d67c0478b9e9
georgiygusev <georgiy.gus@yandex.ru>
2025-07-20 15:06:04 +0300

    set standard filed ts

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ MINIO_BUCKET = os.getenv("MINIO_BUCKET")
 async def handle_message(msg):
     task_id = msg["task_id"]
     api_token = msg["wb_token"]
-    load_date = msg["ts"]
+    ts = msg["ts"]
 
     logging.info(f"Start processing task {task_id}")
 
     data = await fetch_data(api_token)
     filename = "prices.json"
-    prefix = f"{load_date}/{task_id}/"
+    prefix = f"{ts}/{task_id}/"
     minio_key = prefix + filename
 
     await upload_to_minio(
@@ -44,7 +44,7 @@ async def handle_message(msg):
 
     return {
         "task_id": task_id,
-        "load_date": load_date,
+        "ts": ts,
         "minio_key": minio_key
     }
 
